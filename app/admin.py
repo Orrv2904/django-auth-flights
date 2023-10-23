710836156c34d40ec54df74ef4c037aa6b10e53d
Orrv2904 <Omarrv2904@gmail.com>
2023-10-22 20:53:46 -0600

    :construction:

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class PassengerInline(admin.StackedInline):
+    model = Passenger.flights.through
+    extra = 1
+    
+class FlightAdmin(admin.ModelAdmin):
+    inlines = [PassengerInline]
+    
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+    
+
+admin.site.register(Airport)
+admin.site.register(flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
